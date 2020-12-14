4b4890af8a25b9b37a112922e891de433ae00038
ajcwebdev <ac@ac.local>
2020-12-14 12:26:54 -0700

    Deploy and database setup

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <>
+      <BlogLayout>
+        About
+      </BlogLayout>
+    </>
+  )
+}
+
+export default AboutPage
